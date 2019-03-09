c9e6c1c0cd24f0dceb30a60197860b19fd97d366
Huayong Kuang <sudohuoty@163.com>
2019-03-09 13:28:06 +0800

    optimize

--- scripts/ctext-huangdi-neijing.py
+++ scripts/ctext-huangdi-neijing.py
@@ -7,30 +7,50 @@
 
 import re
 import json
+import time
+import random
+import logging
+import logging as log
+
 import requests
 from pyquery import PyQuery
 
 
+session = requests.Session()
+session.headers["User-Agent"] = " ".join([
+    "Mozilla/5.0",
+    "(Windows NT 10.0; Win64; x64)",
+    "AppleWebKit/537.36",
+    "(KHTML, like Gecko)",
+    "Chrome/67.0.3396.99 Safari/537.36",
+])
+
+
+def random_time_sleep():
+    time.sleep(random.random.uniform(2, 15))
+
+
 def parse_text_page(url):
     """解析文本内容页面"""
-    resp = requests.get(url)
+    resp = session.get(url)
     doc = PyQuery(resp.content)
     text_list = []
+    log.info("parsing %s", url)
     for item in doc('tr td.ctext').items():
         item_html = item.html()
         if not re.search(r'<div.*</p>', item_html):
             continue
         item_text = PyQuery(item_html).text()
         text_list.append(item_text.strip())
-
     return text_list
 
 
 def parse_contents_page(url, book_name):
     """解析书籍目录页面"""
-    resp = requests.get(url)
+    resp = session.get(url)
     doc = PyQuery(resp.content)
     contents = {}
+    log.info("parsing %s", url)
     for item in doc('#content3 a').items():
         a_href = item.attr('href')
         if not a_href.startswith(book_name):
@@ -40,6 +60,22 @@ def parse_contents_page(url, book_name):
 
 
 def main():
+    session.get("https://ctext.org/")
+    random_time_sleep()
+    session.headers["Referer"] = "https://ctext.org/"
+    session.get("https://ctext.org/zhs")
+    random_time_sleep()
+    session.headers["Referer"] = "https://ctext.org/zhs"
+    session.get("https://ctext.org/pre-qin-and-han/zhs")
+    random_time_sleep()
+    session.headers["Referer"] = "https://ctext.org/pre-qin-and-han/zhs"
+    session.get("https://ctext.org/huangdi-neijing/zhs")
+    random_time_sleep()
+    session.headers["Referer"] = "https://ctext.org/huangdi-neijing/zhs"
+    session.get("https://ctext.org/huangdi-neijing/suwen/zhs")
+    random_time_sleep()
+    session.headers["Referer"] = "https://ctext.org/huangdi-neijing/suwen/zhs"
+
     json_file = "../data/json/huangdi-neijing-suwen.json"
     content_page = "https://ctext.org/huangdi-neijing/suwen/zhs"
     data = {
@@ -58,6 +94,7 @@ def main():
             text_data = {"ID": text_idx, "text": text}
             section_data['data'].append(text_data)
             text_idx += 1
+            random_time_sleep()
         data['alldata'].append(section_data)
 
     print(data)
@@ -66,4 +103,8 @@ def main():
 
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s -  %(message)s'
+    )
     main()
